5bd137928c8b26b649042a8780825e0f2dd5a805
Антон Катаев <anton@MacBook-Air-Anton.local>
2025-12-24 11:54:20 +0300

    добавлен tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon']
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon'
+  ]
 })
