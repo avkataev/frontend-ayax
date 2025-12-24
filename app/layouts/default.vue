f69cf5a280e8a48e1e0e37a3472e9e35a5c4b9e8
Антон Катаев <anton@MacBook-Air-Anton.local>
2025-12-24 11:44:54 +0300

    базовая архитектура

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import Sidebar from "~/shared/components/sidebar.vue";
+</script>
+
+<template>
+<div>
+  Layout
+  <sidebar />
+  <slot/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
